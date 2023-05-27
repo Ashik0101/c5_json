e97c5dc1c8feabb81e9996eeddeeba050a18ac98
Md Ashik Ansari <aashiqmohd04@gmail.com>
2023-05-27 13:27:49 +0530

    changed cors code

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("./db.json");
 const middlewares = jsonServer.defaults();
-const cors = require("cors");
+// const cors = require("cors");
 
-server.use(cors());
-
+// server.use(cors());
+server.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 server.use(middlewares);
 server.use(router);
 
